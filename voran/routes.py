59f15982142e6002430cb988bdd087544eae21fc
Charles C <chas@charlescorbett.com>
2014-02-27 16:52:35 -0500

    Semantics fix
    yep

--- voran/routes.py
+++ voran/routes.py
@@ -1,7 +1,12 @@
+from voran import app
 from flask import Flask
+
+appName = "Voran Playground"
 
 @app.route("/")
 def hello():
 	return "Testing"
+
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
