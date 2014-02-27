9d35567e21262cd84595b3cefd9aaaee67c0c8b9
Charles C <chas@charlescorbett.com>
2014-02-27 17:09:39 -0500

    Style and render templates
    Styling and render templates added

--- voran/routes.py
+++ voran/routes.py
@@ -1,11 +1,12 @@
 from voran import app
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 
 appName = "Voran Playground"
 
 @app.route("/")
-def hello():
-	return "Testing"
+def home():
+	appName = "Voran Playground"	
+	return render_template('home.html', title=appName)
 
 
 if __name__ == "__main__":
